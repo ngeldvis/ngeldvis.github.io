175d97f44b200a8c65014c33c890b5df914dccb2
Nigel Davis <ngeldvis@gmail.com>
2021-05-10 23:36:17 -0400

    minor changes

--- assets/src/app/app.py
+++ assets/src/app/app.py
@@ -1,9 +1,10 @@
 import django
 import os
-assets\src\app\app\settings.py
+import json
+
 from dotenv import load_dotenv
 from github import Github
-import github
+
 
 #
 # note: backend not yet connected to the hosted website
@@ -33,3 +34,18 @@ def get_desc(g: Github, name: str) -> str:
 # returns a list of repo topics
 def get_topics(g: Github, name: str) -> list:
     return g.get_user().get_repo(name).get_topics()
+
+def get_repo_url(g: Github, name: str) -> list:
+    return g.get_user().get_repo(name).html_url
+
+
+info = []
+repos = get_repos(g)
+
+for repo in repos:
+    desc = get_desc(g, repo)
+    topics = get_topics(g, repo)
+    url = get_repo_url(g, repo)
+    info.append({'name': repo, 'desc': desc, 'url': url, 'topics': topics})
+
+print(json.dumps(info))
